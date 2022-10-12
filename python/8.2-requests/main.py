fe6b681417ca82048bbcd3943bdbd4e85eaa4b9a
Jesus <jesus@jesusyepes.com>
2022-10-12 21:16:26 +0200

    ejercicio 8.2-requests hecho.

---
+++ python/8.2-requests/main.py
@@ -0,0 +1,16 @@
+import requests
+
+headers = {"Authorization": "Bearer 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"}
+result = []
+
+for i in range(1,6):
+  endpoint = "https://api.github.com/search/repositories?q=language:python+stars:>5000&sort=stars&order=desc&per_page=100&page=" + str(i)
+  res = requests.get(endpoint, headers=headers).json()
+  items = res.get('items')
+  result = result + items
+
+for repo in result:
+  print(repo.get('name') + " - " + str(repo.get("stargazers_count")))
+
+print("***********")
+print("Total repositorios: " + str(len(result)))
